d0bb687e845c462d7261be39fc9834afc63d7958
Omar Hammad <omar.hammad@student.kdg.be>
2025-09-20 14:20:43 +0200

    project initialization

---
+++ src/main/java/com/omarhammad/kdg_backend/KdgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.omarhammad.kdg_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdgBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omarhammad/kdg_backend/KdgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarhammad.kdg_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
