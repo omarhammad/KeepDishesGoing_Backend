25a2cae4f7586417d7b17baab5e1525ad0691e74
Omar Hammad <omar.hammad@student.kdg.be>
2025-09-20 15:13:24 +0200

    add @Modulith on the main class with logging all avaliable modules

--- src/main/java/com/omarhammad/kdg_backend/KdgBackendApplication.java
+++ src/main/java/com/omarhammad/kdg_backend/KdgBackendApplication.java
@@ -1,13 +1,28 @@
 package com.omarhammad.kdg_backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.modulith.Modulith;
+import org.springframework.modulith.core.ApplicationModules;
 
-@SpringBootApplication
+@Modulith
 public class KdgBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KdgBackendApplication.class, args);
-	}
+    private Logger log = LoggerFactory.getLogger(KdgBackendApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdgBackendApplication.class, args);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    void onApplicationStarted(){
+        ApplicationModules modules = ApplicationModules.of(KdgBackendApplication.class);
+        modules.forEach(module -> log.info("\n{}",module));
+    }
 
 }
+
+
